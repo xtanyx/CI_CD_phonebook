b4cc334cc4b55badf91f2cc828244a630c677842
xtanyx <xpawartanishqx@gmail.com>
2024-11-20 23:17:24 +0530

    pipeline check testing 0.3

--- frontend/test/Filter.test.jsx
+++ frontend/test/Filter.test.jsx
@@ -1,9 +1,12 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
+// eslint-disable-next-line no-unused-vars
 import Filter from '../src/components/Filter'
 
+// eslint-disable-next-line no-undef
 test('handle is called when filter text is changed', async () => {
 
+  // eslint-disable-next-line no-undef
   const mockHandler = vi.fn()
   const user = userEvent.setup()
 
@@ -13,5 +16,6 @@ test('handle is called when filter text is changed', async () => {
 
   await user.type(input, 'testing')
 
+  // eslint-disable-next-line no-undef
   expect(mockHandler.mock.calls).toHaveLength(7)
 })
--- frontend/test/Notification.test.jsx
+++ frontend/test/Notification.test.jsx
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react'
+// eslint-disable-next-line no-unused-vars
 import Notification from '../src/components/Notification'
 
+// eslint-disable-next-line no-undef
 test('notification message is rendered', () => {
   const message = 'something is rendered'
   const isSuccess = true
@@ -8,5 +10,6 @@ test('notification message is rendered', () => {
   render(<Notification message={message} isSuccess={isSuccess}/>)
 
   const element = screen.getByText(`${message}`)
+  // eslint-disable-next-line no-undef
   expect(element).toBeDefined()
 })
