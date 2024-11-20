3069d86ac81e3e0374a892d252cc24903b513697
xtanyx <xpawartanishqx@gmail.com>
2024-11-20 16:50:07 +0530

    pipeline check build 0.16

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,7 @@ export default [
       globals: {
         ...globals.browser,
         ...globals.node,
-        ...globals.es2015
+        ...globals
       },
       ecmaVersion: "latest",
       parserOptions: {
